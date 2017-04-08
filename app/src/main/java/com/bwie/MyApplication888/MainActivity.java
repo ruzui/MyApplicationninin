ae31e8b86d3fe6cb91e146417032ba100d77827e
dell <1727226822@>
2017-04-08 08:11:42 +0800

    第一次提交亲

--- app/src/main/java/com/bwie/MyApplication888/MainActivity.java
+++ app/src/main/java/com/bwie/MyApplication888/MainActivity.java
@@ -2,14 +2,13 @@ package com.bwie.MyApplication888;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //hmfgchymkcfg,rd,zsl
+        findViewById(R.id.);
     }
 }
